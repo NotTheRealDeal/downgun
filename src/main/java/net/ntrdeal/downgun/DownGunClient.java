c7a6d209814df7811d1e70b7e0dfe67525ad3222
RealDeal <realthedealthe2nd@gmail.com>
2025-12-29 17:12:13 -0700

    Changed some things

---
+++ src/main/java/net/ntrdeal/downgun/DownGunClient.java
@@ -0,0 +1,9 @@
+package net.ntrdeal.downgun;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DownGunClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
